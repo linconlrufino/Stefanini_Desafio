b94abbbed4a6d7a62d93727569039481a31eaa28
Linconl Rufino <62862403+linconlrufino@users.noreply.github.com>
2022-10-06 09:39:49 -0300

    Add files via upload

---
+++ DesafioApi/Infra/Repositories/CidadeRepository.cs
@@ -0,0 +1,107 @@
+﻿using Application.Queries;
+using Domain.Entities;
+using Domain.Repositories;
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositories;
+
+public class CidadeRepository : ICidadeRepository
+{
+    private readonly DesafioContext _context;
+
+    public CidadeRepository(DesafioContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Create(Cidade cidade)
+    {
+        try
+        {
+            await _context.Cidades.AddAsync(cidade);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        try
+        {
+            var cidade = await _context.Cidades.FirstOrDefaultAsync(x => x.Id == id);
+
+            _context.Cidades.Remove(cidade);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<IEnumerable<Cidade>> GetAll()
+    {
+        try
+        {
+            return await _context.Cidades
+                        .AsNoTracking()
+                        .OrderBy(x => x.UF).ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<Cidade> GetById(int id)
+    {
+        try
+        {
+            return await _context.Cidades
+                      .FirstOrDefaultAsync(CidadeQueries.GetById(id));
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<Cidade> GetByName(string nome)
+    {
+        try
+        {
+            return await _context.Cidades
+                      .FirstOrDefaultAsync(CidadeQueries.GetByName(nome));
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<bool> Update(Cidade cidade)
+    {
+        try
+        {
+            _context.Update(cidade);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
---
+++ DesafioApi/Infra/Repositories/PessoaRepository.cs
@@ -0,0 +1,107 @@
+﻿using Application.Queries;
+using Domain.Entities;
+using Domain.Repositories;
+using Infra.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infra.Repositories;
+
+public class PessoaRepository : IPessoaRepository
+{
+    private readonly DesafioContext _context;
+
+    public PessoaRepository(DesafioContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Create(Pessoa pessoa)
+    {
+        try
+        {
+            await _context.Pessoas.AddAsync(pessoa);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        try
+        {
+            var pessoa = await _context.Pessoas
+                            .FirstOrDefaultAsync(PessoaQueries.GetById(id));
+
+            _context.Pessoas.Remove(pessoa);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<IEnumerable<Pessoa>> GetAll()
+    {
+        try
+        {
+            return await _context.Pessoas.Include(x => x.Cidade)
+                        .OrderBy(x => x.Id).ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<Pessoa> GetById(int id)
+    {
+        try
+        {
+            return await _context.Pessoas.Include(x => x.Cidade)
+                      .FirstOrDefaultAsync(PessoaQueries.GetById(id));
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<bool> Update(Pessoa pessoa)
+    {
+        try
+        {
+            _context.Update(pessoa);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task<bool> VerificarSeCpfExiste(string cpf)
+    {
+        try
+        {
+            return await _context.Pessoas.AnyAsync(x => x.CPF == cpf);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
